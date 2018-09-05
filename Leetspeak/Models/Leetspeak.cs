568cda86a0688ed75ffa8d26fdf2b27c90d2364f
Catherine Bradley and Meria Thomas <catherine@>
2018-09-05 16:16:56 -0700

    add condition for s and add regex

--- Leetspeak/Models/Leetspeak.cs
+++ Leetspeak/Models/Leetspeak.cs
@@ -1,61 +1,61 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
 
-// namespace Leetspeak
-//  {
+
+
+namespace Leetspeak
+ {
    public class LeetspeakTranslator
    {
-     public char letter;
-     public char Translate(char letter)
+
+     public string Translate(string input)
      {
-       if(letter == 'e')
+       Regex rx = new Regex(@"^[a-zA-Z]+$");
+       List<char> inputList = new List<char> { };
+       List<char> outputList = new List<char> { };
+
+       for(int i = 0; i < input.Length; i++)
        {
-         return '3';
+         inputList.Add(input[i]);
+         if (inputList[i] == 'e')
+         {
+           outputList.Add('3');
+         }
+         else if(inputList[i] == 'o')
+         {
+           outputList.Add('0');
+         }
+         else if(inputList[i] == 't')
+         {
+           outputList.Add('7');
+         }
+         else if(inputList[i] == 'I')
+         {
+           outputList.Add('1');
+         }
+         else if(i > 1 && rx.IsMatch(inputList[i - 1].ToString()) && ((inputList[i] == 's') || (inputList[i] == 'S')))
+         {
+           outputList.Add('z');
+         }
+         else
+         {
+           outputList.Add(inputList[i]);
+         }
        }
-       else if(letter == 'o')
-       {
-         return '0';
-       }
-       else if(letter == 't')
-       {
-         return '7';
-       }
-       else if(letter == 'I')
-       {
-         return '1';
-       }
-       else
-       {
-         return letter;
-       }
+       string translatedOutput = string.Join("", outputList.ToArray());
+       return translatedOutput;
      }
-   }
- // }
-
-
- class Program
- {
-   public static void Main()
-   {
-     Console.WriteLine("Please enter a sentence");
-     string input = Console.ReadLine();
-
-     char[] array = input.ToCharArray();
-     LeetspeakTranslator myTranslator = new LeetspeakTranslator();
-     List<char> resultArr = new List<char>{};
-     for(int i = 0; i < array.Length; i++)
-     {
-       char letter = array[i];
-       resultArr.Add(myTranslator.Translate(letter));
-      //  output = myTranslator.Translate(letter)
-      //  string strOutput = string.Join("", output.ToArray());
-      //  Console.WriteLine(strOutput);
-
-     }
-     Console.WriteLine("Input Sentence: " + input);
-     Console.WriteLine(resultArr.ToArray());
-
-
-
    }
  }
+
+
+ // class Program
+ // {
+ //   public static void Main()
+ //   {
+ //
+ //
+ //   }
+ // }
